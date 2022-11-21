6c18275688fb1ac9acbed167665fc841f8bff9b4
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-21 17:46:00 +0900

    Update win.sh

--- win.sh
+++ win.sh
@@ -3,6 +3,6 @@
 PASS='sshpass -p camac476357! ssh Administrator@13.209.89.118 -o StrictHostKeyChecking=no'
 PASS2='sshpass -p camac476357! scp test.html Administrator@13.209.89.118:c:/'
 
-
+sudo apt install -y sshpass
 
 $PASS DIR 
