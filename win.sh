bf123f7f6bead0a00fe94c7d558b85b62a0ed7b6
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-21 17:40:31 +0900

    Update win.sh

--- win.sh
+++ win.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-PASS='sshpass "camac476357!" ssh Administrator@13.209.89.118'
+PASS=sshpass -p camac476357! ssh Administrator@13.209.89.118 -o StrictHostKeyChecking=no
+PASS2=sshpass -p camac476357! scp test.html Administrator@13.209.89.118:c:/
 
-sudo apt insatll -y sshpass
 
-$PASS
 
-DIR
-ipconfig
+$PASS DIR 
