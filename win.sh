e7c7ca4ec453b960723b8636c60b1faf7f9aea36
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-22 09:45:55 +0900

    Update win.sh

--- win.sh
+++ win.sh
@@ -6,5 +6,6 @@ TEST2='sshpass -p camac476357! scp test3.tar Administrator@15.164.164.104:c:/'
 $TEST dir 'c:\'
 $TEST dir 'c:\inetpub\wwwroot'
 $TEST2
+$TEST dir 'c:\'
 $TEST tar -xvf c:/test3.tar -C 'c:\inetpub\wwwroot'
 $TEST dir 'c:\inetpub\wwwroot'
